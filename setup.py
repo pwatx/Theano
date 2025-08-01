f0ea809baee4224102ec2460f68ec9293ecfa840
Synix <pwatx@sina.com>
2025-08-01 21:43:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,11 @@ Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
+Programming Language :: Python :: 3.11
+Programming Language :: Python :: 3.12
+
 """
 NAME                = 'Theano'
 MAINTAINER          = "PyMC devs"
